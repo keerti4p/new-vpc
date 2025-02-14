4ee3f7ccc2e204109632e766333145de16081ec2
keerti4p <keerti4p+1@gmail.com>
2025-02-14 06:46:43 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+# Terraform provider configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
